2e26f0361a6f6385bcbc560b656d0946c40b5263
roryschram <schror002@myuct.ac.za>
2022-09-11 15:51:41 +0200

    random

--- Progress Report 01/Code/main.py
+++ Progress Report 01/Code/main.py
@@ -3,49 +3,69 @@ import lz4.frame
 from sys import getsizeof
 from pathlib import Path
 import Timing
+from encrypt import AESCipher
+import pyaes, pbkdf2, binascii, os, secrets
 
-sOut = "Lines of Data,Compress Time (s),Decompress Time (s),Original Size (B),Compressed Size (B),Compression Ratio"
-with open("experiment.csv", "w") as f:
-    f.write(sOut+"\n")
-
-for j in range(15,34000,342):
-    data = ""
-
-    # Import the raw data as a string called "data".
-    with open('data.csv') as f:
-        lines = f.readlines()
-    #data = "".join(lines)
-
-    for i in range(0,j):
-        data += lines[i]
-
+def compress(data):
     # Convert imported csv data to bytes format.
     data_as_bytes = str.encode(data)
 
     # Compress the data.
     Timing.startlog()
     compressed = lz4.frame.compress(data_as_bytes)
+    global iTimeToCompress
     iTimeToCompress = Timing.endlog()
 
-    # Decompress the data.
-    Timing.startlog()
-    decompressed = lz4.frame.decompress(compressed)
-    iTimeToDecompress = Timing.endlog()
-
-    # Print out the sizes of the data, compressed and not compressed.
+    # Get the sizes of the data, compressed and not compressed.
+    global iSizeOriginal
     iSizeOriginal = getsizeof(data_as_bytes)
+
+    global iSizeCompressed
     iSizeCompressed = getsizeof(compressed)
+
+    global iCompressionRatio
+    iCompressionRatio = iSizeOriginal/iSizeCompressed
 
     # Write the binary compressed data to a file.
     with open('compressed_data.txt', 'wb') as f:
         f.write(compressed)
 
-    # Read in the compressed data file.
-    lines = ""
-    lines = Path('compressed_data.txt').read_bytes()
+def encrypt(data):
+    # Derive a 256-bit AES encryption key from the password
+    password = "s3cr3t*c0d3"
+    passwordSalt = os.urandom(16)
+    key = pbkdf2.PBKDF2(password, passwordSalt).read(32)
 
-    # Decompress this data.
-    decompressed = lz4.frame.decompress(lines)
+    # Encrypt the data.
+    Timing.startlog()
+    encrypted = aes.encrypt(data)
+
+    global iTimeToEncrypt
+    iTimeToEncrypt = Timing.endlog()
+
+    # Write the binary compressed encrypted data to a file.
+    with open('compressed_encrypted_data.txt', 'wb') as f:
+        f.write(encrypted)
+
+def decrypt(data):
+
+    # Decrypt the data.
+    Timing.startlog()
+    decrypted = aes.decrypt(data)
+    global iTimeToDecrypt
+    iTimeToDecrypt = Timing.endlog()
+
+    # Write the binary compressed decrypted data to a file.
+    with open('compressed_decrypted_data.txt', 'wb') as f:
+        f.write(decrypted)
+
+
+def decompress(data):
+    # Decompress the data.
+    Timing.startlog()
+    decompressed = lz4.frame.decompress(data)
+    global iTimeToDecompress
+    iTimeToDecompress = Timing.endlog()
 
     #Convert back from byte array.
     decompressed = decompressed.decode()
@@ -55,35 +75,61 @@ for j in range(15,34000,342):
         f.write(decompressed)
 
 
-    iCompressionRatio = iSizeOriginal/iSizeCompressed
 
-    """print("Time to compress: "+str(iTimeToCompress))
-    print("Time to decompress: "+str(iTimeToDecompress))
-    print("Original size: "+str(iSizeOriginal))
-    print("Compressed size: "+str(iSizeCompressed))
-    print("Compression Ratio: "+str(iCompressionRatio))"""
+def main():
 
-    sOut = str(j)+","+str(iTimeToCompress)+","+str(iTimeToDecompress)+","+str(iSizeOriginal)+","+str(iSizeCompressed)+","+str(iCompressionRatio)
-    with open("experiment.csv", "a") as f:
+    # Write the header of the experimental data file.
+    sOut = "Lines of Data,Compress Time (s),Encrypt Time (s),Decrypt Time (s),Decompress Time (s),Original Size (B),Compressed Size (B),Compression Ratio,Comparison Ratio"
+    with open("experiment.csv", "w") as f:
         f.write(sOut+"\n")
 
+    # Control the number if times the experiment runs. 
+    for j in range(15,34000,3400):
+        
+        # Import the raw data as a string called "data".
+        data = ""
+        with open('data.csv') as f:
+            lines = f.readlines()
 
-    """
-    # Import the original raw data as a string called "data".
-    with open('data.csv') as f:
-        lines = f.read()
-    data = "".join(lines)
+        for i in range(0,j):
+            data += repr(lines[i])
+        
+        compress(data)
 
-    # Import the decompressed data as a string called "decompressed_data".
-    with open('decompressed_data.csv') as f:
-        lines = f.read()
-    decompressed_data = "".join(lines)
+        # Read in the compressed data file.
+        lines = ""
+        lines = Path('compressed_data.txt').read_bytes()
+        decompress(lines)
+
+        """
+        # Get the compressed data and create a string from it
+        with open("compressed_data.txt","rb") as f:
+            fileArray = f.readlines()
+    
+        data = ""
+
+        for line in fileArray:
+            data += repr(line)
+        encrypt(data)"""
+
+        
+        """
+        # Get the compressed encrypted data and create a string from it
+        with open("compressed_encrypted_data.txt","rb") as f:
+            fileArray = f.readlines()
+    
+        data = ""
+
+        for line in fileArray:
+            data += str(line)
+        decrypt(data)"""
 
 
-    # Check if the decompressed data is the same as the original data.
-    if (data == decompressed_data): 
-        print("The data is the exact same!")
-    else:
-        print("There is an issue and some of the data does not match!")
 
-    """
+        sOut = str(j)+","+str(iTimeToCompress)+","+str(iTimeToEncrypt)+","+str(iTimeToDecompress)+","+str(iSizeOriginal)+","+str(iSizeCompressed)+","+str(iCompressionRatio)
+        with open("experiment.csv", "a") as f:
+            f.write(sOut+"\n")
+
+
+if __name__ == "__main__":
+    main()
--- Progress Report 01/Code/main1.py
+++ Progress Report 01/Code/main1.py
@@ -1,47 +1,21 @@
-from pathlib import Path
-from encrypt import AESCipher
-import Timing
+import pyaes, pbkdf2, binascii, os, secrets
 
-def main():
-    aes = AESCipher('dsabjdbasjkdbasjkdba')
+# Derive a 256-bit AES encryption key from the password
+password = "s3cr3t*c0d3"
+passwordSalt = os.urandom(16)
+key = pbkdf2.PBKDF2(password, passwordSalt).read(32)
+print('AES encryption key:', binascii.hexlify(key))
 
-    with open("compressed_data.txt","rb") as f:
-        fileArray = f.readlines()
+# Encrypt the plaintext with the given key:
+#   ciphertext = AES-256-CTR-Encrypt(plaintext, key, iv)
+iv = secrets.randbits(256)
+plaintext = "Text for encryption\n"
+aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
+ciphertext = aes.encrypt(plaintext)
+print('Encrypted:', binascii.hexlify(ciphertext))
 
-
-    lines = ""
-
-    for line in fileArray:
-        lines += str(line)
-
-
-    
-    """# Converting byte text file to string text file ---------------------------
-    lines = ""
-    lines = Path('compressed_data.txt').read_bytes()
-    str(lines)"""
-
-    # Encrypt the data.
-    Timing.startlog()
-    encrypted = aes.encrypt(lines)
-    iTimeToEncrypt = Timing.endlog()
-
-    print(encrypted)
-
-    # Decrypt the data.
-    Timing.startlog()
-    decrypted = aes.decrypt(encrypted)
-    iTimeToDecrypt = Timing.endlog()
-
-
-    # string = "I am hungry and should use classes and objects to lay out my project"
-
-    # encrypted_string = aes.encrypt(string)
-    # print(encrypted_string)
-
-    # decrypted_string = aes.decrypt(encrypted_string)
-    # print(decrypted_string)
-
-
-if __name__ == "__main__":
-    main()
+# Decrypt the ciphertext with the given key:
+#   plaintext = AES-256-CTR-Decrypt(ciphertext, key, iv)
+aes = pyaes.AESModeOfOperationCTR(key, pyaes.Counter(iv))
+decrypted = aes.decrypt(ciphertext)
+print('Decrypted:', decrypted)
